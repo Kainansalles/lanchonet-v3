be92db491427a8c9578498d01f9d0445c2daf771
Kainan Salles Araújo <kainansalles@outlook.com>
2018-10-28 00:59:32 -0300

    Primeiro commit do layout novo

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <title>{{ config('app.name', 'Laravel') }}</title>
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta charset="utf-8">
+    <meta name="description" content="LanchoNET">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!--begin::Web font -->
+    <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.16/webfont.js"></script>
+    <script>
+      WebFont.load({
+        google: {"families":["Poppins:300,400,500,600,700","Roboto:300,400,500,600,700"]},
+        active: function() {
+            sessionStorage.fonts = true;
+        }
+      });
+    </script>
+
+	<link href="{{ asset('css/vendors.bundle.css') }}" rel="stylesheet" type="text/css" />
+	<link href="{{ asset('css/style.bundle.css') }}" rel="stylesheet" type="text/css" />
+</head>
+
+<body  class="m-page--fluid m--skin- m-content--skin-light2 m-header--fixed m-header--fixed-mobile m-aside-left--enabled m-aside-left--skin-dark m-aside-left--offcanvas m-footer--push m-aside--offcanvas-default"  >
+		<!-- begin:: Page -->
+		<div class="m-grid m-grid--hor m-grid--root m-page">
+			<!-- BEGIN: Header -->
+            @include('partials.header')
+			<!-- END: Header -->
+		<!-- begin::Body -->
+			<div class="m-grid__item m-grid__item--fluid m-grid m-grid--ver-desktop m-grid--desktop m-body">
+	<!-- BEGIN: Left Aside -->
+				<button class="m-aside-left-close  m-aside-left-close--skin-dark " id="m_aside_left_close_btn">
+					<i class="la la-close"></i>
+                </button>
+                @include('partials.menu')
+                @yield('content')
+</div>
+<!-- end:: Body -->
+<!-- begin::Footer -->
+@include('partials.footer')
+<!-- end::Footer -->
+</div>
+<!-- end:: Page -->
+	<!-- begin::Scroll Top -->
+<div id="m_scroll_top" class="m-scroll-top">
+	<i class="la la-arrow-up"></i>
+</div>
+</body>
+
+<!--begin::Base Scripts -->
+<script src="{{ asset('js/vendors.bundle.js') }}" type="text/javascript"></script>
+<script src="{{ asset('js/scripts.bundle.js') }}" type="text/javascript"></script>
+<!--end::Base Scripts -->
+</html>
